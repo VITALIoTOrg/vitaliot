ecc9cf1abd6cd2f53b11a5144966abf996faf91b
Lorenzo bracco <l.bracco@reply.it>
2015-06-08 15:36:30 +0200

    Add an API to get a user's groups

--- vital-core-security-adapter/src/main/java/securitywrapper/GetServices.java
+++ vital-core-security-adapter/src/main/java/securitywrapper/GetServices.java
@@ -1,6 +1,8 @@
 package securitywrapper;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import jsonpojos.Groups;
 import jsonpojos.User;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -38,6 +41,43 @@ public class GetServices {
 		
 		try {
 			answer = JsonUtils.serializeJson(client.getUser(userId));
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return Response.ok()
+				.entity(answer)
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+				.build();
+	}
+	
+	@Path("/user/{id}/groups")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUserGroups(@PathParam("id") String userId){
+		
+		String answer;
+		
+		answer = null;
+		
+		Groups groups = client.getGroups();
+		List<String> list = groups.getResult();
+		Iterator<String> iter = list.listIterator();
+		while (iter.hasNext()) {
+			String group = iter.next();
+			if(!client.userIsInGroup(userId, group)) {
+				iter.remove();
+			}
+		}
+		groups.setResult(list);
+		
+		try {
+			answer = JsonUtils.serializeJson(groups);
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
