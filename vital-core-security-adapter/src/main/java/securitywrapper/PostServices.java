da876181221a1292d8ab3a052f053ca9c51670a3
Lorenzo bracco <l.bracco@reply.it>
2015-06-09 15:44:47 +0200

    Implement API to remove user from group

--- vital-core-security-adapter/src/main/java/securitywrapper/PostServices.java
+++ vital-core-security-adapter/src/main/java/securitywrapper/PostServices.java
@@ -255,11 +255,52 @@ public class PostServices {
 		}
 	}
 	
-	@Path("/group/delUser")
+	@Path("/group/{id}/delUser")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public String removeUserToGroup() {
-		return null;
+	public Response removeUserFromGroup(
+			@PathParam("id") String groupId,
+			@FormParam("user") String username) {
+		String answer;
+		
+		answer = null;
+		
+		ArrayList<String> usersList = new ArrayList<String>();
+		usersList.add(username);
+		
+		if(client.deleteUsersFromGroup(groupId, usersList)) {
+			try {
+				answer = JsonUtils.serializeJson(client.getGroup(groupId));
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return Response.ok()
+					.entity(answer)
+					.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Credentials", "true")
+					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+					.build();
+		} else {
+			try {
+				answer = JsonUtils.serializeJson(client.getGroup(groupId));
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return Response.status(Status.BAD_REQUEST)
+					.entity(answer)
+					.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Credentials", "true")
+					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+					.build();
+		}
 	}
 	
 	@Path("/policy/create")
