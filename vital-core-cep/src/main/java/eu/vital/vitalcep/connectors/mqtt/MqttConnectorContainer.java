144c45201a266417e8ee9aabfbd66750ce4cc364
Elisa Herrmann <elisa.herrmann@atos.net>
2016-03-03 14:28:03 +0100

    added singeton container

--- vital-core-cep/src/main/java/eu/vital/vitalcep/connectors/mqtt/MqttConnectorContainer.java
+++ vital-core-cep/src/main/java/eu/vital/vitalcep/connectors/mqtt/MqttConnectorContainer.java
@@ -5,27 +5,36 @@ import java.util.HashMap;
 
 public class MqttConnectorContainer {
 
-	HashMap<String, MqttConnector> MqttContainer;
+	static public HashMap<String, MQTT_conn_interface> MqttContainer;
 	
-	public MqttConnectorContainer() {
-		MqttContainer = new HashMap<String, MqttConnector>();
-	}
-	
-	public MqttConnector getConnector (String name){
-		return MqttContainer.get(name);
-	}
-	
-	public boolean addConnector (String name, MqttConnector conector){
-		if (MqttContainer.put(name, conector) != null)
-			return true; //FIX ME, this means that there was previous connector with the same name
+	private MqttConnectorContainer() {
 		
-		return true;
 	}
 	
-	public boolean deleteConnector (String name){
-		if (MqttContainer.remove(name)!=null)
-			return true;
-		
-		return false;
+	static public MQTT_conn_interface getConnector (String name){
+            if (MqttContainer == null)
+                MqttContainer = new HashMap<>();
+            
+            return MqttContainer.get(name);
+	}
+	
+	static public boolean addConnector (String name, MQTT_conn_interface conector){
+            if (MqttContainer == null)
+                MqttContainer = new HashMap<>();
+            
+            if (MqttContainer.put(name, conector) != null)
+                    return true; //FIX ME, this means that there was previous connector with the same name
+
+            return true;
+	}
+	
+	static public boolean deleteConnector (String name){
+            if (MqttContainer == null)
+                MqttContainer = new HashMap<>();
+            
+            if (MqttContainer.remove(name)!=null)
+                    return true;
+
+            return false;
 	}
 }
