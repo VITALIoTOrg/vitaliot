ab3741da75767259cf98c3aca75a483b6b8b47f5
Lorenzo Bracco <l.bracco@reply.it>
2016-03-10 17:49:20 +0100

    Use common configuration file

--- vital-core-security-adapter/src/main/java/utils/ConfigReader.java
+++ vital-core-security-adapter/src/main/java/utils/ConfigReader.java
@@ -1,32 +1,33 @@
 package utils;
 
-import java.io.InputStream;
 import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class ConfigReader {
 	
 	private static ConfigReader instance;
 	private Properties config;
 	
-	public static final String IDP_HOST = "IDP_HOST";
-	public static final String IDP_PORT = "IDP_PORT";
-    public static final String IDP_PATH = "IDP_PATH";
-	public static final String PROXY_HOST = "PROXY_HOST";
-	public static final String PROXY_PORT = "PROXY_PORT";
-	public static final String SNMP_PORT = "SNMP_PORT";
-	public static final String SSO_TOKEN = "SSO_TOKEN";
-	public static final String ALT_TOKEN = "ALT_TOKEN";
+	public static final String IDP_HOST = "vital-core-security-adapter.idp-host";
+	public static final String IDP_PORT = "vital-core-security-adapter.idp-port";
+    public static final String IDP_PATH = "vital-core-security-adapter.idp-path";
+	public static final String PROXY_HOST = "vital-core-security-adapter.proxy-host";
+	public static final String PROXY_PORT = "vital-core-security-adapter.proxy-port";
+	public static final String SSO_TOKEN = "vital-core-security-adapter.sso-token";
+	public static final String ALT_TOKEN = "vital-core-security-adapter.alt-token";
 	
 	private ConfigReader() {
-		config = new Properties();
-		InputStream is = this.getClass().getResourceAsStream("/config.properties");
+        String fileName = System.getProperty("jboss.server.config.dir") + "/vital-properties.xml";
+        File file = new File(fileName);
+
+        config = new Properties();
 		
 		try {
-			config.load(is);
+			config.loadFromXML(new FileInputStream(file));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public static ConfigReader getInstance() {
@@ -39,6 +40,4 @@ public class ConfigReader {
 	public String get(String key) {
 		return config.getProperty(key);
 	}
-	
-	
 }
