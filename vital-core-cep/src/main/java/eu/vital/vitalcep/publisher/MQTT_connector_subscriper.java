96ece0b438e4b75d67a1a9d19894be318e65af41
Elisa Herrmann <elisa.herrmann@atos.net>
2016-03-16 09:42:36 +0100

    renamed

---
+++ vital-core-cep/src/main/java/eu/vital/vitalcep/publisher/MQTT_connector_subscriper.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.vital.vitalcep.publisher;
+
+import eu.vital.vitalcep.connectors.mqtt.MQTT_conn_interface;
+import eu.vital.vitalcep.connectors.mqtt.MqttConnector;
+import eu.vital.vitalcep.connectors.mqtt.MsgQueue;
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ *
+ * @author a601149
+ */
+public class MQTT_connector_subscriper implements MQTT_conn_interface{
+    
+    public MqttConnector connector;
+    private String clientName;
+    private String queueName;
+    
+    public MQTT_connector_subscriper(String queueName, 
+            MessageProcessor_publisher MsgProcc){
+        
+        if (connector == null){
+            this.queueName = queueName;
+            clientName = "publisher_"+RandomStringUtils.randomAlphanumeric(4);
+            MsgQueue oQueue = new MsgQueue(MsgProcc);
+            MqttConnector connector = new MqttConnector(clientName, 
+                     oQueue,"", queueName, 2);
+            this.connector = connector;
+       }
+    }
+    
+    public String getClientName(){
+        return clientName;
+    }
+    public String getQueuename(){
+        return queueName;
+    }
+}
