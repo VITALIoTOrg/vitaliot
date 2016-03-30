9e6cc874a3c702abe1be1fce186f4722a7b662a4
Elisa Herrmann <elisa.herrmann@atos.net>
2016-03-30 14:11:08 +0200

    added try chatch structure

--- vital-core-cep/src/main/java/eu/vital/vitalcep/collector/listener/PPIListener.java
+++ vital-core-cep/src/main/java/eu/vital/vitalcep/collector/listener/PPIListener.java
@@ -5,12 +5,7 @@
  */
 package eu.vital.vitalcep.collector.listener;
 
-import eu.vital.vitalcep.cep.CEP;
-import eu.vital.vitalcep.cep.CepProcess;
 import eu.vital.vitalcep.conf.PropertyLoader;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import eu.vital.vitalcep.connectors.dms.DMSManager;
 import eu.vital.vitalcep.connectors.ppi.PPIManager;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -21,6 +16,7 @@ import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -46,7 +42,6 @@ public class PPIListener {
         this.cookie= cookie; 
                 
     }
-      
     
     public JSONArray getObservations(JSONArray requests,
             String from ) throws IOException, UnsupportedEncodingException,
@@ -55,30 +50,31 @@ public class PPIListener {
         JSONObject completequery = new JSONObject();
 
         JSONArray aData = new JSONArray();
-
-             
+            
         for (int j = 0; j < requests.length(); j++) {
+            
+            try{
+                JSONObject simplequery = new JSONObject();
 
-            JSONObject simplequery = new JSONObject();
+                simplequery = requests.getJSONObject(j)
+                        .getJSONObject("body").put("from",from);
 
-            simplequery = requests.getJSONObject(j)
-                    .getJSONObject("body").put("from",from);
+                PPIManager oPPI = new PPIManager(cookie);
 
-            PPIManager oPPI = new PPIManager(cookie);
-
-            aData = oPPI.getObservations(requests.getJSONObject(j)
-                    .getString("ppiURL"),simplequery
-                    .toString());
-
+                aData = oPPI.getObservations(requests.getJSONObject(j)
+                        .getString("ppiURL"),simplequery
+                        .toString());
+            }catch(Exception ex){
+                java.util.logging.Logger.getLogger(PPIListener
+                                    .class.getName())
+                                        .log(Level.SEVERE, null, ex);
+            }
         }
        
         return aData;
     
     }
     
-    
-     
-
     private String getXSDDateTime(Date date) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
         return  dateFormat.format(date);
