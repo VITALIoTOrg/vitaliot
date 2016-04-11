f0e4de9e31c2260a8e0045a3d880c1fbd38e22c0
Lorenzo Bracco <l.bracco@reply.it>
2016-04-11 17:33:04 +0200

    Use spaces instead of tabs

--- vital-core-security-snmp-exposer/src/main/java/NotFoundExceptionMapper.java
+++ vital-core-security-snmp-exposer/src/main/java/NotFoundExceptionMapper.java
@@ -8,12 +8,12 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class NotFoundExceptionMapper implements ExceptionMapper<javax.ws.rs.NotFoundException> {
-	@Produces(MediaType.TEXT_HTML)
-	public Response toResponse(NotFoundException exception) {
-		String answer;
-		
-		answer = "<html><head><title>404 NOT FOUND</title></head><body><h1>404 RESOURCE NOT FOUND</h1></body></html>";
-		
-		return Response.status(Status.NOT_FOUND).entity(answer).build();
+    @Produces(MediaType.TEXT_HTML)
+    public Response toResponse(NotFoundException exception) {
+        String answer;
+        
+        answer = "<html><head><title>404 NOT FOUND</title></head><body><h1>404 RESOURCE NOT FOUND</h1></body></html>";
+        
+        return Response.status(Status.NOT_FOUND).entity(answer).build();
     }
 }
--- vital-core-security-snmp-exposer/src/main/java/clients/OpenAMClient.java
+++ vital-core-security-snmp-exposer/src/main/java/clients/OpenAMClient.java
@@ -20,84 +20,84 @@ public class OpenAMClient {
     private ConfigReader configReader;
     private String snmpHost;
     private int snmpPort;
-	
-	public OpenAMClient() {
+    
+    public OpenAMClient() {
         configReader = ConfigReader.getInstance();
         
         snmpHost = configReader.get(ConfigReader.SNMP_HOST);
         snmpPort = Integer.parseInt(configReader.get(ConfigReader.SNMP_PORT));
-	}
-	
-	public String getStatValue(String oidValue) {
-		
-		String answer = null;
+    }
+    
+    public String getStatValue(String oidValue) {
+        
+        String answer = null;
 
-		int snmpVersion  = SnmpConstants.version2c;
-		String community = "public";
+        int snmpVersion  = SnmpConstants.version2c;
+        String community = "public";
 
-	    // Create TransportMapping and Listen
-	    TransportMapping transport = null;
-		try {
-			transport = new DefaultUdpTransportMapping();
-			transport.listen();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        // Create TransportMapping and Listen
+        TransportMapping transport = null;
+        try {
+            transport = new DefaultUdpTransportMapping();
+            transport.listen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	    // Create Target Address object
-	    CommunityTarget comtarget = new CommunityTarget();
-	    comtarget.setCommunity(new OctetString(community));
-	    comtarget.setVersion(snmpVersion);
-	    comtarget.setAddress(new UdpAddress(snmpHost + "/" + snmpPort));
-	    comtarget.setRetries(3);
-	    comtarget.setTimeout(2000);
+        // Create Target Address object
+        CommunityTarget comtarget = new CommunityTarget();
+        comtarget.setCommunity(new OctetString(community));
+        comtarget.setVersion(snmpVersion);
+        comtarget.setAddress(new UdpAddress(snmpHost + "/" + snmpPort));
+        comtarget.setRetries(3);
+        comtarget.setTimeout(2000);
 
-	    // Create the PDU object
-	    PDU pdu = new PDU();
-	    pdu.add(new VariableBinding(new OID(oidValue)));
-	    pdu.setType(PDU.GET);
-	    pdu.setRequestID(new Integer32(1));
+        // Create the PDU object
+        PDU pdu = new PDU();
+        pdu.add(new VariableBinding(new OID(oidValue)));
+        pdu.setType(PDU.GET);
+        pdu.setRequestID(new Integer32(1));
 
-		// Create SNMP object for sending data to Agent
-		Snmp snmp = new Snmp(transport);
+        // Create SNMP object for sending data to Agent
+        Snmp snmp = new Snmp(transport);
 
-		ResponseEvent response = null;
-		try {
-			response = snmp.get(pdu, comtarget);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        ResponseEvent response = null;
+        try {
+            response = snmp.get(pdu, comtarget);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// Process Agent Response
-		if(response != null) {
-			PDU responsePDU = response.getResponse();
-			if(responsePDU != null) {
-				int errorStatus = responsePDU.getErrorStatus();
-		        String errorStatusText = responsePDU.getErrorStatusText();
+        // Process Agent Response
+        if(response != null) {
+            PDU responsePDU = response.getResponse();
+            if(responsePDU != null) {
+                int errorStatus = responsePDU.getErrorStatus();
+                String errorStatusText = responsePDU.getErrorStatusText();
 
-		        if(errorStatus == PDU.noError) {
-		        	answer = responsePDU.getVariableBindings().firstElement().toString();
-		        	String delims = "[=]";
-		        	answer = answer.split(delims)[1].substring(1);
-		        }
-		        else {
-		        	answer = "{ \"message\": \"" + errorStatusText + "\" }";
-		        }
-			}
-		    else {
-		    	answer = "{ \"message\": \"" + "Response PDU is null" + "\" }";
-		    }
-		}
-		else {
-			answer = "{ \"message\": \"" + "Agent Timeout" + "\" }";
-		}
-		try {
-			snmp.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return answer;   
-	}
+                if(errorStatus == PDU.noError) {
+                    answer = responsePDU.getVariableBindings().firstElement().toString();
+                    String delims = "[=]";
+                    answer = answer.split(delims)[1].substring(1);
+                }
+                else {
+                    answer = "{ \"message\": \"" + errorStatusText + "\" }";
+                }
+            }
+            else {
+                answer = "{ \"message\": \"" + "Response PDU is null" + "\" }";
+            }
+        }
+        else {
+            answer = "{ \"message\": \"" + "Agent Timeout" + "\" }";
+        }
+        try {
+            snmp.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return answer;   
+    }
 
 }
